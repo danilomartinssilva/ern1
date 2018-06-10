16117ce4aeea723549a1f534f60fdafed6a0e37b
Danilo Martins <danilomartinssilva@r7.com>
2018-06-10 12:41:40 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+
+//import Dashboard from './Dashboard';
+
+import Routes from './Routes'
+console.disableYellowBox = true;
+AppRegistry.registerComponent('expenses', () => Routes);
